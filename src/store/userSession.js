96045c1d574e282d7f6b37c408ef433c7ba9aaf5
marcosherna <marcosxdf6@gmail.com>
2025-10-22 14:28:28 -0600

    build: persisten user session

--- src/store/userSession.js
+++ src/store/userSession.js
@@ -1,16 +1,26 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const useUserSession = create((set, get) => ({
-  user: null, // { name, email, password }
+export const useUserSession = create(
+  persist(
+    (set, get) => ({
+      user: null, // { name, email, password }
 
-  setUser: (userData) => set({ user: userData }),
+      setUser: (userData) => set({ user: userData }),
 
-  clearUser: () => set({ user: null }),
+      clearUser: () => set({ user: null }),
 
-  getUserCollectionPrefix: () => {
-    const user = get().user;
-    if (!user?.name) return "desconocido";
-    const parts = user.name.trim().toLowerCase().split(" ");
-    return parts.slice(0, 2).join("_");
-  },
-}));
+      getUserCollectionPrefix: () => {
+        const user = get().user;
+        if (!user?.name) return "desconocido";
+        const parts = user.name.trim().toLowerCase().split(" ");
+        return parts.slice(0, 2).join("_");
+      },
+    }),
+    {
+      name: "user-session-storage",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
